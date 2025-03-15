4a497d75c97c2c3cdb31256c8c6f068cc77bf65e
khangdaibang <pandorakhang@gmail.com>
2025-03-15 12:53:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dlu_messenger/login.java
@@ -0,0 +1,87 @@
+package com.example.dlu_messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+
+    Button button;
+    EditText email, password;
+    FirebaseAuth auth;
+    String emailPatTern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logbutton);
+        email = findViewById(R.id.editTextLogEmail);
+        password = findViewById(R.id.editTextLogPassword);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if((TextUtils.isEmpty(Email))) {
+                    Toast.makeText(login.this, "Enter The Email", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(pass)) {
+                    Toast.makeText(login.this, "Enter The Password", Toast.LENGTH_SHORT).show();
+                } else if (!Email.matches(emailPatTern)) {
+                    email.setError("Give Proper Email Address");
+                } else if (password.length() < 6) {
+                    password.setError("More Then Six Character");
+                    Toast.makeText(login.this, "Password Needs To Be Longer Then Six Character", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(Email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                try {
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }catch (Exception e) {
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/dlu_messenger/registration.java
@@ -0,0 +1,185 @@
+package com.example.dlu_messenger;
+
+import android.app.ComponentCaller;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class registration extends AppCompatActivity {
+
+    TextView loginBut;
+    EditText rg_username, rg_email, rg_password, rg_repassword;
+    Button rg_signup;
+    CircleImageView rg_profileImg;
+    FirebaseAuth auth;
+    Uri imageURI;
+    String imageuri;
+    String emailPatTern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        auth = FirebaseAuth.getInstance();
+        loginBut = findViewById(R.id.loginBut);
+        rg_username = findViewById(R.id.rgusserName);
+        rg_email = findViewById(R.id.rgEmail);
+        rg_password = findViewById(R.id.rgPassword);
+        rg_repassword = findViewById(R.id.rgrePassword);
+        rg_profileImg = findViewById(R.id.profilerg);
+        rg_signup = findViewById(R.id.signupbutton);
+
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(registration.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        rg_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = rg_username.getText().toString();
+                String emaill = rg_email.getText().toString();
+                String Password = rg_password.getText().toString();
+                String cPassword = rg_repassword.getText().toString();
+                String status = "Hey I'm Using This Application";
+                
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(emaill) ||
+                        TextUtils.isEmpty(Password) || TextUtils.isEmpty(cPassword)) {
+                    Toast.makeText(registration.this, "Please Enter Valid Information", Toast.LENGTH_SHORT).show();
+                } else if (!emaill.matches(emailPatTern)) {
+                    rg_email.setError("Type A Valid Email Here");
+                } else if (Password.length() < 6) {
+                    rg_password.setError("Password Must Be 6 Character Or More");
+                } else if (!Password.equals(cPassword)) {
+                    rg_password.setError("The Password Doesn't Match");
+                } else {
+                    auth.createUserWithEmailAndPassword(emaill, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String id = task.getResult().getUser().getUid();
+                                DatabaseReference reference = database.getReference().child("user").child(id);
+                                StorageReference storageReference = storage.getReference().child("Upload").child(id);
+
+                                if (imageURI != null) {
+                                    storageReference.putFile(imageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        imageuri = uri.toString();
+                                                        Users users = new Users(id, name, emaill, Password, cPassword, imageuri);
+                                                        reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    Intent intent = new Intent(registration.this, MainActivity.class);
+                                                                    startActivity((intent));
+                                                                    finish();
+                                                                } else {
+                                                                    Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            } else {
+                                                String status = "Hey I'm Using This Application";
+                                                imageuri = "https://www.creativefabrica.com/wp-content/uploads/2022/07/04/Female-user-avatar-Generic-app-profile-Graphics-33541506-1.png";
+                                                Users users = new Users(id, name, emaill, Password, imageuri, status);
+                                                reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Intent intent = new Intent(registration.this, MainActivity.class);
+                                                            startActivity((intent));
+                                                            finish();
+                                                        } else {
+                                                            Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                }
+                            } else {
+                                Toast.makeText(registration.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+        rg_profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 10);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data, @NonNull ComponentCaller caller) {
+        super.onActivityResult(requestCode, resultCode, data, caller);
+        if (requestCode == 10) {
+            if (data != null) {
+                imageURI = data.getData();
+                rg_profileImg.setImageURI(imageURI);
+
+            }
+
+        }
+    }
+}
